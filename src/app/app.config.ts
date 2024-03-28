34a1a3745fb6b7c62bbdb769f0cbf95bad296c9a
bosss09 <visotskiy11@gmail.com>
2024-03-28 12:43:16 +0000

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,38 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {LuxonDateAdapter} from "@angular/material-luxon-adapter";
 
-import { routes } from './app.routes';
+import {PreloadAllModules, provideRouter, withInMemoryScrolling, withPreloading} from '@angular/router';
+import { appRoutes } from './app.routes';
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {provideHttpClient} from "@angular/common/http";
+import {DateAdapter, MAT_DATE_FORMATS} from "@angular/material/core";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideAnimations(),
+    provideHttpClient(),
+    provideRouter(appRoutes,
+      withPreloading(PreloadAllModules),
+      withInMemoryScrolling({scrollPositionRestoration: 'enabled'}),
+      ),
+    // Material Date Adapter
+    {
+      provide : DateAdapter,
+      useClass: LuxonDateAdapter,
+    },
+    {
+      provide : MAT_DATE_FORMATS,
+      useValue: {
+        parse  : {
+          dateInput: 'D',
+        },
+        display: {
+          dateInput         : 'DDD',
+          monthYearLabel    : 'LLL yyyy',
+          dateA11yLabel     : 'DD',
+          monthYearA11yLabel: 'LLLL yyyy',
+        },
+      },
+    },
+  ],
 };
